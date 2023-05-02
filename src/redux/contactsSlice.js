6bbb4dbb20129d484d65a16fce1b6a0e2afc7135
Denys07748 <denis07748@gmail.com>
2023-05-02 14:54:15 +0300

    delete Persist & del use localStorage

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,12 +1,10 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 const initialState = {
   value: [],
 };
 
-const contactsSlice = createSlice({
+export const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
   reducers: {
@@ -18,15 +16,5 @@ const contactsSlice = createSlice({
     },
   },
 });
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-export const contactsReducer = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
-);
 
 export const { addContact, deleteContact } = contactsSlice.actions;
