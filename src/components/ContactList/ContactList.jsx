ab8b81e73c5d8723a5312ef73fd0d59240279a06
Denys07748 <denis07748@gmail.com>
2023-05-07 14:33:18 +0300

    adds selectVisibleContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,16 @@
 import { ContactsList } from './ContactList.styled';
 import ContactListItem from './ContactListItem';
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
-
-const getVisibleContacts = (contacts, filter) => {
-  const normalizedFilter = filter.toLowerCase();
-
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
-};
+import { selectVisibleContacts } from 'redux/selectors';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-  const visibleContacts = getVisibleContacts(contacts, filter);
+  const contacts = useSelector(selectVisibleContacts);
 
-  return visibleContacts.length === 0 ? (
+  return contacts.length === 0 ? (
     <p>Сontacts not found</p>
   ) : (
     <ContactsList>
-      {visibleContacts.map(({ id, name, phone }) => (
+      {contacts.map(({ id, name, phone }) => (
         <ContactListItem key={id} id={id} name={name} phone={phone} />
       ))}
     </ContactsList>
